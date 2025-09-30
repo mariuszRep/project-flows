c9858222bf02066ca4b2ddf61418d3c02e4626a2
Mariusz <mariuszrepczynski@gmail.com>
2025-09-30 14:23:41 +0100

    refactor: remove NewTemplateForm component and related template management code

--- ui/src/pages/Template.tsx
+++ ui/src/pages/Template.tsx
@@ -4,7 +4,7 @@ import { useSession } from '@/contexts/SessionContext';
 import { useNavigate } from 'react-router-dom';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import NewTemplateForm from '@/components/forms/NewTemplateForm';
+import NewTemplateForm from '@/components/forms/TemplateForm';
 import { useMCP } from '@/contexts/MCPContext';
 
 interface Template {
